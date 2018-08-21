85596530283acbb3c76320eada045d5bac57ca72
Javier Justiniano <javitospfv@gmail.com>
2018-08-21 17:03:50 -0400

    Slider finished

--- Maquetado/wwwroot/js/site.js
+++ Maquetado/wwwroot/js/site.js
@@ -1,1 +1,34 @@
 ﻿// Write your JavaScript code.
+var slideIndex = 1;
+$(document).ready(function () {  
+    showSlides(slideIndex);
+});
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    console.log(slideIndex);
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    console.log(slides);
+    var dots = document.getElementsByClassName("dot");
+    console.log(dots);
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" actives", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " actives";
+} 
